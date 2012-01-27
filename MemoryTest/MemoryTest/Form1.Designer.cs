075d15fa1b147cc63fb0ab544176652babb15887
Yuriy Samoylenko <samoylenko.yuriy@gmail.com>
2012-01-27 23:03:03 +0200

    Start condition.

---
+++ MemoryTest/MemoryTest/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MemoryTestWF
+{
+    partial class MemoryTestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MemoryTestForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonAbort = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(65, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(276, 52);
+            this.label1.TabIndex = 0;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label2.Location = new System.Drawing.Point(331, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Delay";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonStart.Location = new System.Drawing.Point(146, 301);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(110, 50);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(151, 66);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TabStop = false;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.numericUpDown.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown.Location = new System.Drawing.Point(331, 371);
+            this.numericUpDown.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(70, 20);
+            this.numericUpDown.TabIndex = 4;
+            this.numericUpDown.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(37, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.buttonsClick);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(104, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.buttonsClick);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(173, 110);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 30);
+            this.button3.TabIndex = 8;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.buttonsClick);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(244, 110);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 30);
+            this.button4.TabIndex = 9;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.buttonsClick);
+            // 
+            // button0
+            // 
+            this.button0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button0.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button0.Location = new System.Drawing.Point(316, 110);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(30, 30);
+            this.button0.TabIndex = 10;
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Visible = false;
+            this.button0.Click += new System.EventHandler(this.buttonsClick);
+            // 
+            // buttonAbort
+            // 
+            this.buttonAbort.Enabled = false;
+            this.buttonAbort.Location = new System.Drawing.Point(164, 357);
+            this.buttonAbort.Name = "buttonAbort";
+            this.buttonAbort.Size = new System.Drawing.Size(75, 23);
+            this.buttonAbort.TabIndex = 11;
+            this.buttonAbort.Text = "Abort";
+            this.buttonAbort.UseVisualStyleBackColor = true;
+            this.buttonAbort.Click += new System.EventHandler(this.buttonAbort_Click);
+            // 
+            // MemoryTestForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Controls.Add(this.buttonAbort);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = this.Size;
+            this.Name = "MemoryTestForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Memory Test";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button buttonStart;
+        private TextBox textBox1;
+        private NumericUpDown numericUpDown;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button0;
+        private Button buttonAbort;
+    }
+}
+
---
+++ MemoryTest/MemoryTest/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MemoryTestWF
+{
+    public partial class MemoryTestForm : Form
+    {
+        /// <summary>
+        /// Inicializes a new instance of the MemoryTest.MemoryTestForm class.
+        /// </summary>
+        public MemoryTestForm()
+        {
+            InitializeComponent();
+            buttons = new Button[] { button0, button1, button2, button3, button4 };
+        }
+
+        private Button[] buttons;//Array for 
+        RandomNumbers randNums = new RandomNumbers();
+        List<int> randNumsList = new List<int>(5);//The list with random numbers.
+        List<int> userNumsList = new List<int>(5);//The list with random numbers entered by user.
+        RandomLocation ranLoc = new RandomLocation();
+        List<List<int>> randomLocation = new List<List<int>>(5);//The list with random X and Y coordinates of location of five elements.
+
+        /// <summary>
+        /// Event handler for buttonStart click.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">An System.EventArgs that contains no event data.</param>
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = false;
+            numericUpDown.Enabled = false;
+            buttonAbort.Enabled = true;
+
+            textBox1.BackColor = this.BackColor;
+            textBox1.Text = "";
+
+            randomLocation = ranLoc.GetRandomArray();
+            randNumsList = randNums.GetRandomNums();//Fills a list of numbers.
+
+            //Displays random numbers on the buttons.
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].ForeColor = Color.Black;
+                buttons[i].Text = Convert.ToString(randNumsList[i]);
+                buttons[i].Location = new Point(randomLocation[i][0], randomLocation[i][1]);
+                buttons[i].Visible = true;
+            }
+
+            Thread myThread = new Thread(MyThread);
+            myThread.Start();
+
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Enabled = true;
+            }
+        }
+
+        /// <summary>
+        /// Event handler for buttons click.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">An System.EventArgs that contains no event data.</param>
+        private void buttonsClick(object sender, EventArgs e)
+        {
+            //Addition the numbers entered by user to the list.
+            userNumsList.Add(Convert.ToInt32(char.GetNumericValue((sender.ToString().Last()))));
+
+            //Verifies the correctness of sequence of numbers entered by user.
+            if (userNumsList.Count == 5)
+            {
+                if (randNums.Equals(userNumsList))
+                {
+                    textBox1.BackColor = Color.LimeGreen;
+                    textBox1.Text = "RIGHT";
+                }
+                else
+                {
+                    textBox1.BackColor = Color.Tomato;
+                    textBox1.Text = "WRONG";
+                }
+
+                Reset();
+            }
+        }
+
+        /// <summary>
+        /// Event handler for buttonAbort click
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">An System.EventArgs that contains no event data.</param>
+        private void buttonAbort_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        /// <summary>
+        /// Parallel thread, performs a delay, after which hides the numbers from the user.
+        /// </summary>
+        public void MyThread()
+        {
+            Thread.Sleep((int)numericUpDown.Value);
+            if (buttons[4].Text != "")
+            {
+                for (int i = 0; i < buttons.Length; i++)
+                {
+                    buttons[i].ForeColor = buttons[i].BackColor;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets controls in the start position.
+        /// </summary>
+        private void Reset()
+        {
+            for (int i = 0; i < buttons.Length; i++)
+            {
+                buttons[i].Enabled = false;
+            }
+            buttonStart.Enabled = true;
+            numericUpDown.Enabled = true;
+            buttonAbort.Enabled = false;
+            userNumsList.Clear();
+            randomLocation.Clear();
+            randNumsList.Clear();
+        }
+    }
+
+    /// <summary>
+    /// Generate a list with random numbers.
+    /// </summary>
+    class RandomNumbers
+    {
+        private Random gen = new Random();
+        List<int> randomNumbersList = new List<int>(5);
+
+        /// <summary>
+        /// Generate a list with random numbers.
+        /// </summary>
+        /// <returns>List with random numbers.</returns>
+        public List<int> GetRandomNums()
+        {
+            randomNumbersList.Clear();
+
+            while (randomNumbersList.Count < 5)
+            {
+                int randomNumber = gen.Next(1, 9);
+
+                if (!randomNumbersList.Contains(randomNumber))
+                {
+                    randomNumbersList.Add(randomNumber);
+                }
+            }
+
+            return randomNumbersList;
+        }
+
+        /// <summary>
+        /// Returns the value indication whether this instance is equal to a specified List.
+        /// </summary>
+        /// <param name="compList">List with int32 numbers.</param>
+        /// <returns>true if lists are equivalent.</returns>
+        public bool Equals(List<int> compList)
+        {
+            randomNumbersList.Sort();
+            for (int i = 0; i < 5; i++)
+            {
+                if (randomNumbersList[i] != compList[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+    }
+
+    /// <summary>
+    /// Generate the list with random X and Y coordinates of location of five elements.
+    /// </summary>
+    class RandomLocation
+    {
+        private Random gen = new Random();
+        private List<int> randomList;
+        private List<List<int>> randomNumList = new List<List<int>>(5);
+        private int randomNumber;
+
+        /// <summary>
+        /// Generate the list with random X and Y coordinates of location of five elements.
+        /// </summary>
+        /// <returns>List with rundom X and Y coordinates of location of five elements.</returns>
+        public List<List<int>> GetRandomArray()
+        {
+            while (randomNumList.Count < 5)
+            {
+                randomList = new List<int>(2);
+
+                randomNumber = gen.Next(0, 12);
+                randomList.Add(randomNumber * 30);
+
+                randomNumber = gen.Next(3, 9);
+                randomList.Add(randomNumber * 30);
+
+                if (Check(randomList))
+                {
+                    randomNumList.Add(randomList);
+                }
+            }
+
+            return randomNumList;
+        }
+
+        /// <summary>
+        /// Check for repeats in the list.
+        /// </summary>
+        /// <param name="ranList">List with int32 numbers.</param>
+        /// <returns>True if the list is exclusive.</returns>
+        private bool Check(List<int> ranList)
+        {
+            foreach (List<int> i in randomNumList)
+            {
+                if ((i[0] == ranList[0]) & (i[1] == ranList[1])) return false;
+            }
+            return true;
+        }
+    }
+}
